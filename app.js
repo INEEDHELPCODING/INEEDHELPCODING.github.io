39261a1838a03d0b07ad8786a6beb0ecdb10a300
Gemmy <gemmylai@gmail.com>
2025-08-13 22:06:38 +1000

    minor adjustment to minutes

--- app.js
+++ app.js
@@ -154,7 +154,7 @@ const totalRounds = 10;  // Very much subject to change
 
 // Minus one because actual time is 1 second longer than specified due to how setInterval works. Always has interval of minimum 1 seconds.
 const intermissionTime = 4 - 1;
-const roundTime = 45 - 1;
+const roundTime = 60 - 1;
 const pointScale = 10;
 const roundMD = 10  // Stat maximum deviation from roundMedian
 const roundMedian = 15
